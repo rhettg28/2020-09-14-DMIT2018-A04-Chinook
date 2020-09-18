ed5d403d13799a6c49e49b02ef3456c40c4656ae
rhettg28 <55155595+rhettg28@users.noreply.github.com>
2020-09-18 15:44:22 -0600

    Fixed Album Entity, added on to it.

--- ChinookSystem/Entities/Album.cs
+++ ChinookSystem/Entities/Album.cs
@@ -13,15 +13,34 @@ namespace ChinookSystem.Entities
     [Table("Albums")]
     internal class Album
     {
+        //private date members
+        private string _ReleaseLabel;
+
         [Key]
         public int AlbumId { get; set; }
 
-        //validation anotation
-        [Required(ErrorMessage = "Artist Name is required")]
-        [StringLength(120, ErrorMessage = "Artist name is limited to 120 characters")]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(160, ErrorMessage = "Title is limited to 160 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "ArtistId is required")]
+        //A foreign key: child of the relationship to the parent Artist
+        //means that a record points to A (Singular) artist record.
+        //DO NOT USE [ForeignKey] anotation IF names are the same
         public int ArtistId { get; set; }
+
+        [Required(ErrorMessage = "Release Year is required")]
         public int ReleaseYear { get; set; }
-        public string ReleaseLabel { get; set; }
+
+        [StringLength(50, ErrorMessage = "Release Label is limited to 50 characters")]
+        public string ReleaseLabel
+        {
+            get { return _ReleaseLabel; }
+            set { _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value; }
+        }
+
+        //navigational properties
+        public virtual Artist Artist { get; set; }
+        //public virtual ICollection<Track> Tracks { get; set; }
     }
 }
