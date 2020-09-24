103cc8ccd1c12edb90b8ba274f69d4ac2d0eb637
rhettg28 <55155595+rhettg28@users.noreply.github.com>
2020-09-24 09:03:57 -0600

    Frontend Working.

--- WebApp/SamplePages/P05_DDLofArtistsToCGVofAlbumsbyArtistId.aspx.designer.cs
+++ WebApp/SamplePages/P05_DDLofArtistsToCGVofAlbumsbyArtistId.aspx.designer.cs
@@ -60,6 +60,15 @@ namespace WebApp.SamplePages
         protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
+        /// ObjectDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ObjectDataSource2;
+
+        /// <summary>
         /// MessageUserControl control.
         /// </summary>
         /// <remarks>
