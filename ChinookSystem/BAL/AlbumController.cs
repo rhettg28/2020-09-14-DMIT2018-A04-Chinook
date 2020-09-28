606a11044a24fdadccad44ac1a3b2ef95af6d508
rhettg28 <55155595+rhettg28@users.noreply.github.com>
2020-09-28 15:33:11 -0600

    BLL Validation

--- ChinookSystem/BAL/AlbumController.cs
+++ ChinookSystem/BAL/AlbumController.cs
@@ -59,6 +59,7 @@ namespace ChinookSystem.BLL
         //REMEMBER to add the DataKeyNames="AlbumId" 
         //attribute to your ListView so that Delete will work
 
+
         [DataObjectMethod(DataObjectMethodType.Insert, false)]
         public void Albums_Insert(AlbumViewModel item)
         {
@@ -111,6 +112,19 @@ namespace ChinookSystem.BLL
                 context.Albums.Remove(existing);
                 context.SaveChanges();
             }
+        }
+        #endregion
+
+        #region BLL Data Validation
+        private void DataValidation(AlbumViewModel item)
+        {
+            if (string.IsNullOrEmpty(item.AlbumTitle))
+                throw new Exception("(BLL) Album Title is required.");
+            else if (item.AlbumTitle.Length == 160)
+                throw new Exception("(BLL) Album Title is over 160 chars.");
+            else if (item.AlbumReleaseYear < 1950 || item.AlbumReleaseYear > DateTime.Today.Year)
+                throw new Exception(string.Format("(BLL) Year {0} is invalid. Make between 1950 and {1}.",
+                    item.AlbumReleaseYear, DateTime.Today.Year));
         }
         #endregion
     }
