8fc3017a884df874818e597dd09eaa748792eb6b
rhettg28 <55155595+rhettg28@users.noreply.github.com>
2020-11-02 16:03:50 -0700

    Added OLTP Web Form with search bars. Fixed ODS Web Page Name on Site Master. Added GenreController and MEdiaTypeController to BLL. Added SelectionList ViewModel.

--- WebApp/SamplePages/P07_ODSRepeater.aspx
+++ WebApp/SamplePages/P07_ODSRepeater.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ODSRepeater.aspx.cs" Inherits="WebApp.SamplePages.ODSRepeater" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="P07_ODSRepeater.aspx.cs" Inherits="WebApp.SamplePages.ODSRepeater" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
     <h1>Repeater using ODS with nested query</h1>
---
+++ WebApp/SamplePages/P08_OLTP_Demo.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class P08_OLTP_Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Tracks_Button_Command(Object sender, System.Web.UI.WebControls.CommandEventArgs e)
+        {
+            TracksBy.Text = e.CommandName;
+            switch (e.CommandName)
+            {
+                case ("Artist"):
+                    if (string.IsNullOrEmpty(ArtistName.Text))
+                        MessageUserControl.ShowInfo("", "ERROR: Select an artist name or part of.");
+                    else
+                        SearchArg.Text = ArtistName.Text;
+                    break;
+                case ("MediaType"):
+                    SearchArg.Text = MediaTypeDDL.SelectedValue;
+                    break;
+                case ("Genre"):
+                    SearchArg.Text = GenreDDL.SelectedValue;
+                    break;
+                case ("Album"):
+                    if (string.IsNullOrEmpty(AlbumTitle.Text))
+                        MessageUserControl.ShowInfo("", "ERROR: Enter an album title or part of.");
+                    else
+                        SearchArg.Text = AlbumTitle.Text;
+                    break;
+            }
+            //TracksSelectionList.DataBind();
+        }
+
+        protected void PlayList_Button_Command(Object sender, System.Web.UI.WebControls.CommandEventArgs e)
+        {
+        }
+
+        #region Error Handling
+        protected void CheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+
+        protected void InsertCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("", "SUCCESS: Album has been added.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void UpdateCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("", "SUCCESS: Album has been updated.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        protected void DeleteCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("", "SUCCESS: Album has been removed.");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+        }
+        #endregion
+    }
+}
